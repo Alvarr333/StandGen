54579bfe54571662ca6a3bdffc6ada9966da9eaa
Alvaro Sala <alvaro28519@gmail.com>
2023-04-22 21:05:53 +0200

    First frontend draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Stand Generator v1.0</h1>
+			<h4>Stand list</h4>
+			<div className="stand-list">
+				<ul className="standSummary">
+				  <li className="stand">StandName1</li>
+					<li className="user">User1</li>
+					<li className="personality">Personality1</li>
+				</ul>
+				<ul className="standSummary">
+			    <li className="stand">StandName2</li>
+					<li className="user">User2</li>
+					<li className="personality">Personality2</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
